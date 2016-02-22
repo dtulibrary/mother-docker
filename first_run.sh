bd1c990a55a378a5f2fae0c377928957c2b0fae6
Viktor Smari <viktorsmari@gmail.com>
2016-02-22 12:01:35 +0100

    Update covers

--- first_run.sh
+++ first_run.sh
@@ -3,6 +3,7 @@ docker-compose run auth-web   rake db:setup
 docker-compose run docdel-web rake db:setup
 docker-compose run findit-web rake db:setup
 docker-compose run sendit-web rake db:setup
+docker-compose run covers-web rake db:setup
 
 
 # copy the config/ settings from puppet?
