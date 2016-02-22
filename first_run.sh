041faf2c416a5ba9beae86f18e481cab35731f53
Viktor Smari <viktorsmari@gmail.com>
2016-02-22 10:22:14 +0100

    Add rake task for sendit first_run script

--- first_run.sh
+++ first_run.sh
@@ -2,6 +2,7 @@
 docker-compose run auth-web   rake db:setup
 docker-compose run docdel-web rake db:setup
 docker-compose run findit-web rake db:setup
+docker-compose run sendit-web rake db:setup
 
 
 # copy the config/ settings from puppet?
