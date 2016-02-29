3c629b0da2aa443afb13471618507e013c3ec4ca
Viktor Smari <viktorsmari@gmail.com>
2016-02-29 10:55:43 +0100

    Add DocStore / bunsho submodule

--- first_run.sh
+++ first_run.sh
@@ -4,6 +4,7 @@ docker-compose run docdel-web rake db:setup
 docker-compose run findit-web rake db:setup
 docker-compose run sendit-web rake db:setup
 docker-compose run covers-web rake db:setup
+docker-compose run docstore-web rake db:setup
 
 
 # copy the config/ settings from puppet?
