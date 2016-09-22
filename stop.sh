1afe13af1bff570c486b13b5024992c18f2ddcbe
Troels Lisberg Nielsen <troels.lisberg@gmail.com>
2016-09-22 15:26:00 +0200

    Add Redmine 2.5 instance

--- stop.sh
+++ stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker-compose kill -s SIGINT
 docker-compose kill -s SIGKILL findit-solr
+docker-compose kill -s SIGKILL redmine
 docker-compose stop -t 20
+docker-compose rm -f redmine
