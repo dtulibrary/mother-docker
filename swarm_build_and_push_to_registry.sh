ac50f4e18bee8f54f8ea7967a5f50a26f3336ca0
viktorsmari <viktorsmari@gmail.com>
2016-09-22 10:09:22 +0200

    Script can now build ALL projects and push them

--- swarm_build_and_push_to_registry.sh
+++ swarm_build_and_push_to_registry.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
 # Call this with project name as a parameter to build it and push to registry
+# Call it with 'all' and all projects will be built
+
+function buildproject {
+  echo "Building project: " $1
+  docker build -t dtu/$1 $1
+  docker tag dtu/$1 localhost:5000/dtu/$1
+  docker push       localhost:5000/dtu/$1
+}
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Pass a project name as a parameter, to build it and push to registry'
-    exit 0
+  echo 'Pass a project name as a parameter, to build it and push to registry'
+  echo 'Use "all" to build all'
+  exit 0
 fi
 
-docker build -t dtu/$1 $1
-docker tag dtu/$1 localhost:5000/dtu/$1
-docker push       localhost:5000/dtu/$1
+if [ $1 = "all" ]; then
+  echo "Building and pushing ALL projects"
+
+  for i in `grep path .gitmodules | sed 's/.*= //'`;
+  do
+    buildproject $i
+  done
+else
+  buildproject $1
+fi
+
+
